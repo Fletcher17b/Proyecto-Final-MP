a0cba86b65f27fea5a04cb2ea3bb2838971ac32d
ArmDelg <134311525+ArmDelg@users.noreply.github.com>
2023-05-31 23:44:05 -0600

    Update funciones.h

--- Armando/funciones.h
+++ Armando/funciones.h
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include"estructura.h"
 #include<string.h>
 #include<stdlib.h>
 
@@ -13,60 +12,40 @@ void cleanscreen() {
     system("cls");
 }
 
-void newProduct(FILE* fileProduct, struct product_template product) {
-    int guardado = 0;
-    
-    if (fileProduct != NULL) {
-        fflush(stdin);
-        system("cls");
-        printf(" - DATOS DEL PRODUCTO -\n\n");
-        
-        printf("ID del producto: ");
-        scanf("%d", product.prodID);
-        fflush(stdin);
-        
-        printf("Nombre: ");
-        scanf("%s", product.nombre, sizeof(product.nombre), stdin);
-        fflush(stdin);
-        
-        printf("Precio: ");
-        scanf("%d", &product.price);
-        fflush(stdin);
-        
-        printf("Descripcion: ");
-        scanf("%s", product.descripcion, sizeof(product.descripcion), stdin);
-        fflush(stdin);
-        
-        printf("Cantidad en central: ");
-        scanf("%d", &product.sub.cantidad_en_centra);
-        fflush(stdin);
-        
-        printf("Cantidad en sucursal 1: ");
-        scanf("%d", &product.sub.cantidad_en_sucursal_1);
-        fflush(stdin);
-        
-        printf("Cantidad en sucursal 2: ");
-        scanf("%d", &product.sub.cantidad_en_sucursal_2);
-        fflush(stdin);
-        
-        printf("Cantidad en sucursal 3: ");
-        scanf("%d", &product.sub.cantidad_en_sucursal_3);
-        fflush(stdin);
-        
-        printf("Cantidad en sucursal 4: ");
-        scanf("%d", &product.sub.cantidad_en_sucursal_4);
-        fflush(stdin);
-        
-        guardado = fwrite(&product, sizeof(struct product_template), 1, fileProduct);
-        
-        if (guardado > 0) {
-            printf("\n¡Los datos del producto han sido guardados con exito!\n");
-        } else {
-            printf("\nERROR: Intentalo nuevamente. Si el error persiste, contacta al Administrador del Sistema.\n");
-        }
-    } else {
-        printf("\nERROR: Verifica que el archivo de productos exista, esté abierto en modo agregar y no este siendo utilizado por otro programa.\n");
+struct Cliente {
+    char nombre[50];
+    char direccion[100];
+    int telefono;
+};
+
+void registroCliente() {
+    struct Cliente nuevoCliente;
+    printf("Ingrese el nombre del cliente: ");
+    scanf("%s", nuevoCliente.nombre);
+
+    printf("Ingrese la direccion del cliente: ");
+    scanf("%s", nuevoCliente.direccion); 
+
+    printf("Ingrese el telefono del cliente: ");
+    scanf("%d", &nuevoCliente.telefono);
+
+    FILE *file;
+    file = fopen("clientes.dat", "a");
+    if (file == NULL) {
+        printf("Error al crear el archivo.\n");
+        exit(1);
     }
-    
-    fclose(fileProduct);
+    fwrite(&nuevoCliente, sizeof(struct Cliente), 1, file);
+    fclose(file);
+}
+
+int eliminarArchivo(const char* nombreArchivo) {
+    int resultado = remove(nombreArchivo);
+    if (resultado == 0) {
+        printf("El archivo \"%s\" ha sido eliminado correctamente.\n", nombreArchivo);
+        return 0;
+    } else {
+        printf("Error al eliminar el archivo \"%s\".\n", nombreArchivo);
+        return -1;
+    }
 }
