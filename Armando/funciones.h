d0e87ff0bf5c010404b632117a51d3e9902d467a
ArmDelg <134311525+ArmDelg@users.noreply.github.com>
2023-06-01 21:48:21 -0600

    Update funciones.h
    funcion de registro de usuario nuevo y si el usuario ya se ha registrado (es decir que el fichero ya existe) el programa te confirma que ya ha sido creado o sea que ya esta registrado

--- Armando/funciones.h
+++ Armando/funciones.h
@@ -15,28 +15,46 @@ void cleanscreen() {
 struct Cliente {
     char nombre[50];
     char direccion[100];
-    int telefono;
+    int telefono[20];
 };
 
 void registroCliente() {
     struct Cliente nuevoCliente;
-    printf("Ingrese el nombre del cliente: ");
+    printf("Ingrese su nombre de usuario: ");
     scanf("%s", nuevoCliente.nombre);
 
-    printf("Ingrese la direccion del cliente: ");
+    printf("Ingrese su direccion: ");
     scanf("%s", nuevoCliente.direccion); 
 
-    printf("Ingrese el telefono del cliente: ");
+    getchar();
+
+    printf("Ingrese su numero: ");
     scanf("%d", &nuevoCliente.telefono);
 
+    char nombreArchivo[100];
+    snprintf(nombreArchivo, sizeof(nombreArchivo), "%s.data", nuevoCliente.nombre);
+
     FILE *file;
-    file = fopen("clientes.dat", "a");
+    file = fopen(nombreArchivo, "a");
     if (file == NULL) {
         printf("Error al crear el archivo.\n");
         exit(1);
     }
     fwrite(&nuevoCliente, sizeof(struct Cliente), 1, file);
     fclose(file);
+}
+
+int buscarArchivo(const char* nombreArchivo) {
+    FILE* file = fopen(nombreArchivo, "r");
+
+    if (file == NULL) {
+        printf("El archivo no existe. Debes registrarte.\n");
+        return 0; 
+    } else {
+        printf("El archivo existe.\n");
+        fclose(file);
+        return 1; 
+    }
 }
 
 int eliminarArchivo(const char* nombreArchivo) {
