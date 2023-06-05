74606800a928cdfb3975c5b38cb667a4f0a34e89
ArmDelg <134311525+ArmDelg@users.noreply.github.com>
2023-06-04 19:34:16 -0600

    Update main.c

--- Armando/main.c
+++ Armando/main.c
@@ -6,23 +6,24 @@ int main() {
     int op;
     printf("-----Bienvenido a Franchyeska Accesorios-----\n");
     printf("-----Somos una tienda de joyeria para mujeres.-----\n");
-    printf("si quieres obtener mas informacion sobre la tienda pulsa 1 de lo contrario pulsa 2.\n");
+    printf("si quieres obtener mas informacion sobre la tienda pulsa 1 de lo contrario pulsa cualquier otra tecla.\n");
     scanf("%d", &op);
     switch (op)
     {
     case 1:
+    limpiarbuffer();
         if (op==1)
         {
             printf("Pulsaste 1, aqui tendras mas informacion de la tienda\n");
-            printf("Nuestra sucursal principal esta ubicada en bello horizonte, para ser exacto de la rotonda de bello horizonte 2 cuadras al sur, 1 cuadra al oeste y media cuadra al sur. Para contactarte con nosotros puedes usar nuestras redes sociales aparecemos como Franchyeska Accesorios en todas las redes o contactate por nuestro whatsapp +505 8112 1134 estaremos encantados de atenderte\n \n");
+            printf("Nuestra sucursal principal esta ubicada en bello horizonte, para ser exacto de la rotonda de bello horizonte 2 cuadras al sur, 1 cuadra al oeste y media cuadra al sur. Para contactarte con nosotros puedes usar nuestras redes sociales aparecemos como Franchyeska Accesorios en todas o contactate por nuestro whatsapp +505 8112 1134 estaremos encantados de atenderte\n \n");
             limpiarbuffer();
         }
         
         break;
     case 2:
-    if (op==2)
+    if (op!=1)
     {
-        printf(" Pulsaste 2, Gracias por visitarnos, continua navegando.\n");
+        printf("Gracias por visitarnos, continua navegando.\n");
     }
     
     default:
@@ -31,20 +32,24 @@ int main() {
 
     int opregistro;
     cleanscreen();
+    limpiarbuffer();
     printf("Eres nuevo usuario o ya has ingresado alguna vez?\n");
     printf("Si eres nuevo pulsa 1, si ya te habias registrado pulsa 2, si quieres eliminar tu registro pulsa 3.\n");
     scanf("%d", &opregistro);
 
     switch (opregistro) {
         case 1:
-            printf("Estamos encantados de tener un nuevo usuario, por favor regístrate para que estés siempre con nosotros\n");
+        limpiarbuffer();
+            printf("Estamos encantados de tener un nuevo usuario, por favor registrate para que estes siempre con nosotros\n");
             registroCliente();
             printf("Registro de cliente completado.\n");
+            cleanscreen();
             break;
         case 2:
             {
+                limpiarbuffer();
                 char nombreArchivo[100];
-                printf("Ingrese el nombre del archivo a buscar (agrega .txt): ");
+                printf("Ingrese su nombre de usuario (agrega .txt): ");
                 scanf("%s", nombreArchivo);
 
                 int resultado = buscarArchivo(nombreArchivo);
@@ -68,8 +73,9 @@ int main() {
 
         }
         default:
-            printf("Por favor, ingresa una opción valida para que podamos continuar\n");
+            printf("Por favor, ingresa una opcion valida para que podamos continuar\n");
             break;
+            cleanscreen();
             }
     
     return 0;
